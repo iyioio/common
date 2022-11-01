dba5e8120156becd3e096206068e31fb541551ac
Scott Vance <scott@iyio.io>
2022-10-31 23:32:54 -0400

    - fixed MemoryStore file name

--- packages/key-value-store/src/index.ts
+++ packages/key-value-store/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/key-value-store-types';
-export * from './lib/MemoryStory';
+export * from './lib/MemoryStore';
 export * from './lib/RouterStore';
 export * from './lib/_ref.key-value-store';
 export * from './lib/_service.key-value-store';
