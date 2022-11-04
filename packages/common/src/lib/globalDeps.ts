0f5c10612cccf9b16e50ac88c110add471fcd4d8
Scott Vance <scott@iyio.io>
2022-11-04 15:48:08 -0400

    - removed unused file

--- packages/common/src/lib/globalDeps.ts
+++
@@ -1,7 +0,0 @@
-
-/**
- * A shared global DependencyContainer. Library functions and classes should avoid using globalDeps
- * an instead accept a reference to a DependencyContainer and allow end application code to supply
- * either a created DependencyContainer instance or globalDeps. Library code may also use globalDeps
- * as default or fallback value.
- */
