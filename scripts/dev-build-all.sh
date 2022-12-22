1e8b4656b8c60ef09e872ba7e3d6ff18839b22ab
Scott Vance <scott@iyio.io>
2022-12-22 13:51:42 -0500

    - The dev-build-all.sh script now generates the project graph before building. This fixes issues caused by clearning the nx cache

--- scripts/dev-build-all.sh
+++ scripts/dev-build-all.sh
@@ -2,5 +2,8 @@
 set -e
 cd "$(dirname "$0")/.."
 
+echo 'Creating project graph'
+node tools/scripts/create-project-graph.mjs
 
+echo 'Building packages in dev mode'
 node tools/scripts/install-dist-local-deps.mjs "$@"
---
+++ tools/scripts/create-project-graph.mjs
@@ -0,0 +1,13 @@
+import { createProjectGraphAsync } from '@nrwl/devkit';
+
+const main=async ()=>{
+    try{
+        const r=await createProjectGraphAsync();
+        process.exit(0);
+    }catch(ex){
+        console.error(ex);
+        process.exit(1);
+    }
+}
+
+main();
