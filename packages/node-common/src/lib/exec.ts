6af4f3a9db2434edd7622316f5176452e9721a1a
Scott Vance <scott@iyio.io>
2023-03-30 09:32:54 -0400

    - Added output options to execAsync

--- packages/node-common/src/lib/exec.ts
+++ packages/node-common/src/lib/exec.ts
@@ -1,30 +1,104 @@
 import { exec } from 'node:child_process';
 
-
-export function execAsync(cmd:string,silent=false,ignoreErrors=false):Promise<string>
+export interface ExecOptions
 {
+    /**
+     * The command to run
+     */
+    cmd:string;
+
+    /**
+     * Disables output to the stdout, stderr and onExecutionError, out is not effected.
+     * @default false
+     */
+    silent?:boolean;
+
+    /**
+     * If true execution errors are ignored
+     * @default false
+     */
+    ignoreErrors?:boolean;
+
+    /**
+     * Callback that receives all output, both std and stderr.
+     */
+    out?:(value:string)=>void;
+
+    /**
+     * Callback that receives stdout
+     * @default console.info
+     */
+    stdout?:(value:string)=>void;
+
+    /**
+     * Callback that receives stderr
+     * @default console.warn
+     */
+    stderr?:(value:string)=>void;
+
+    /**
+     * Callback that receives execution errors, NOT stderr.
+     * @default console.error
+     */
+    onExecutionError?:(value:string)=>void;
+}
+
+interface execAsyncOverrides
+{
+    (options:ExecOptions):Promise<string>;
+    (cmd:string,silent?:boolean,ignoreErrors?:boolean):Promise<string>;
+}
+
+export const execAsync:execAsyncOverrides=(
+    cmdOrOptions:string|ExecOptions,
+    _silent:boolean=false,
+    _ignoreErrors:boolean=false
+):Promise<string>=>{
+    const options:ExecOptions=typeof cmdOrOptions === 'string'?{
+        cmd:cmdOrOptions,
+        silent:_silent,
+        ignoreErrors:_ignoreErrors,
+    }:cmdOrOptions;
+
+    const {
+        cmd,
+        out,
+        silent=out?true:false,
+        ignoreErrors=false,
+        stdout=console.info,
+        stderr=console.warn,
+        onExecutionError=console.error,
+    }=options;
+
     return new Promise<string>((r,j)=>{
         if(!silent){
-            console.info('> '+cmd);
+            stdout('> '+cmd);
         }
-        exec(cmd,(error,stdout,stderr)=>{
-            if(error){
+        out?.('> '+cmd)
+        exec(cmd,(err,_stdout,_stderr)=>{
+            if(err){
                 if(!silent){
-                    console.error('| '+error);
+                    onExecutionError('| '+err);
                 }
+                out?.('| '+err);
                 if(ignoreErrors){
                     r('');
                 }else{
-                    j(error);
+                    j(err);
                 }
             }else{
                 if(!silent){
-                    console.info('| '+stdout);
-                    if(stderr){
-                        console.warn('| '+stderr);
-                    }
+                    stdout('| '+_stdout);
                 }
-                r(stdout?.trim()||'');
+                out?.('| '+_stdout);
+                if(_stderr){
+                    if(!silent){
+                        stderr('| '+_stderr);
+                    }
+                    out?.('| '+_stderr);
+                }
+
+                r(_stdout?.trim()||'');
             }
         })
     })
