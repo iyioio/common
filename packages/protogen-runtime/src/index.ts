f8c3162480d8baa3fa69c594fbe748e9aeaa1447
Scott Vance <scott@iyio.io>
2023-03-30 09:01:43 -0400

    - removed protogen-cli-entry export

--- packages/protogen-runtime/src/index.ts
+++ packages/protogen-runtime/src/index.ts
@@ -2,7 +2,7 @@ export * from './lib/fileReader';
 export * from './lib/fileWriter';
 export * from './lib/lucidCsvParser';
 export * from './lib/markdownParser';
-export * from './lib/protogen-cli-entry';
 export * from './lib/protogen-runtime';
 export * from './lib/runProtogenCliAsync';
 export * from './lib/zodGenerator';
+
