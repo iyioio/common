346e9b6da638591e2fc08d4cca57c4837d65c759
Scott Vance <scott@iyio.io>
2023-04-21 00:56:50 -0400

    - site builder

---
+++ packages/cdk-common/src/lib/SiteBuilder.ts
@@ -0,0 +1,53 @@
+import { Construct } from "constructs";
+import { ManagedProps } from "./ManagedProps";
+import { StaticWebSite, StaticWebSiteProps } from "./StaticWebSite";
+
+export interface SiteBuilderProps
+{
+    sites:SiteInfo[];
+    managed?:ManagedProps;
+}
+
+export interface SiteInfo
+{
+    name:string;
+    staticSite?:StaticWebSiteProps;
+    modify?:(info:SiteInfo)=>void;
+}
+
+export interface SiteResult
+{
+    info:SiteInfo;
+    staticSite?:StaticWebSite;
+}
+
+export class SiteBuilder extends Construct
+{
+    public readonly sites:SiteResult[];
+
+    public constructor(scope:Construct, name:string, {
+        sites,
+    }:SiteBuilderProps)
+    {
+
+        super(scope,name);
+
+        const results:SiteResult[]=[];
+
+
+        for(const info of sites){
+
+           info.modify?.(info);
+
+            const staticSite=info.staticSite?new StaticWebSite(this,info.name,info.staticSite):undefined;
+
+            results.push({
+                info,
+                staticSite,
+            })
+
+        }
+
+        this.sites=results;
+    }
+}
