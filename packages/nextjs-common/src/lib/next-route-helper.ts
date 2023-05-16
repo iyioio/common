f5b4834753b63b94cdcf57c7197204c51b6e63d6
Scott Vance <scott@iyio.io>
2023-05-16 03:01:17 -0400

    - Added query key count to getRouteInfo for next router because the query was chaning independently of the key

--- packages/nextjs-common/src/lib/next-route-helper.ts
+++ packages/nextjs-common/src/lib/next-route-helper.ts
@@ -1,12 +1,15 @@
-import { RouteInfo } from "@iyio/common";
+import { RouteInfo, getObjKeyCount } from "@iyio/common";
 import { NextRouter } from "next/router";
 
 export const getRouteInfo=(router:NextRouter):RouteInfo=>{
+
+    const q=router.query??{}
+
     return {
-        key:(router.isReady?'':'not-ready-')+(router as any)._key,
+        key:(router.isReady?'':'not-ready-')+(router as any)._key+'_'+getObjKeyCount(q),
         path:router.pathname,
         asPath:router.asPath,
         route:router.route,
-        query:{...(router.query??{})},
+        query:{...q},
     }
 }
