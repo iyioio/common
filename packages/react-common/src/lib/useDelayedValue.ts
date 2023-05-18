edcaea5ea346fce041ab956d0217ccd68161abc1
Scott Vance <scott@iyio.io>
2023-05-18 07:50:08 -0400

    - Added a start value to useDelayedValue

--- packages/react-common/src/lib/useDelayedValue.ts
+++ packages/react-common/src/lib/useDelayedValue.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 
-export const useDelayedValue=<T>(value:T,delayMs:number,noDelayFor?:T):T=>{
+export const useDelayedValue=<T>(value:T,delayMs:number,noDelayFor?:T,startValue?:T):T=>{
 
-    const [val,setVal]=useState(value);
+    const [val,setVal]=useState(startValue===undefined?value:startValue);
 
     useEffect(()=>{
         if(value===noDelayFor){
