956e6616f6529b64d68360a310cd2dd46033a5f3
Scott Vance <scott@iyio.io>
2023-05-21 18:50:52 -0400

    - Added share domain and changed frontend domain default value

--- packages/iyio-common/src/lib/common-params.ts
+++ packages/iyio-common/src/lib/common-params.ts
@@ -3,4 +3,5 @@ import { defineStringParam } from "./scope-lib";
 export const paramsCheckParam=defineStringParam('paramsCheck');
 export const appNameParam=defineStringParam('appName','App');
 export const appLongNameParam=defineStringParam('appLongName','App Long');
-export const frontendDomainParam=defineStringParam('frontendDomain','frontend.com');
+export const frontendDomainParam=defineStringParam('frontendDomain',globalThis.window?.location?.host??'localhost');
+export const shareDomainParam=defineStringParam('shareDomain');
