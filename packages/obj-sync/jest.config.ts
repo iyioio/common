07bbd245056a092b5c2b48117f522105eee45a57
Scott Vance <scott@iyio.io>
2023-05-25 11:26:45 -0400

    - init obj-sync

---
+++ packages/obj-sync/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+    displayName: 'obj-sync',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\.[tj]s$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.spec.json' },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/packages/obj-sync',
+};
