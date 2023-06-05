a8bd60389223c671581f14911f65ca4eb4df8f24
Scott Vance <scott@iyio.io>
2023-06-05 18:29:18 -0400

    - useRouteQuery returns a record of strings.   - useRouteQueryMulti can be used to get string arrays if needed

--- packages/react-common/src/lib/useRoute.ts
+++ packages/react-common/src/lib/useRoute.ts
@@ -13,9 +13,27 @@ export const useRoute=():RouteInfo=>
     },[route]);
 }
 
-export const useRouteQuery=():HashMap<string|string[]|undefined>=>{
+export const useRouteQueryMulti=():HashMap<string|string[]|undefined>=>{
     const {query}=useRoute();
     return query;
+}
+export const useRouteQuery=():HashMap<string>=>{
+    const {query}=useRoute();
+    return useMemo(()=>{
+        const q:Record<string,string>={}
+        if(!query){
+            return q;
+        }
+        for(const e in query){
+            const v=query[e];
+            if(Array.isArray(v)){
+                q[e]=v[0]??'';
+            }else{
+                q[e]=v??'';
+            }
+        }
+        return q;
+    },[query])
 }
 
 export const RouteCtx=createContext<RouteInfo|null>(null);
