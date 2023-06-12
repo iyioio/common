5b199ac719b673f382820b2ba87397909d2f668c
Scott Vance <scott@iyio.io>
2023-06-12 12:07:39 -0400

    - use subject now works with Observables

--- packages/react-common/src/lib/rxjs-hooks.ts
+++ packages/react-common/src/lib/rxjs-hooks.ts
@@ -1,21 +1,25 @@
 import { ReadonlySubject } from "@iyio/common";
 import { useEffect, useState } from 'react';
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
 
 export function useSubject(subject:undefined):undefined;
 export function useSubject<T>(subject:ReadonlySubject<T>):T
 export function useSubject(subject:undefined):undefined;
 export function useSubject<T>(subject:ReadonlySubject<T>|undefined):T|undefined;
-export function useSubject<T>(subject:ReadonlySubject<T>|undefined):T|undefined
+export function useSubject<T>(subject:Observable<T>):T|undefined;
+export function useSubject<T>(subject:ReadonlySubject<T>|Observable<T>|undefined):T|undefined
 {
-    const [value,setValue]=useState<T|undefined>(()=>subject?.value);
+    const [value,setValue]=useState<T|undefined>(()=>(subject as any)?.value);
 
     useEffect(()=>{
         if(!subject){
             setValue(undefined);
             return;
         }
-        setValue(subject.value);
+        const val=(subject as any).value;
+        if(val!==undefined){
+            setValue(val);
+        }
         const sub=subject?.subscribe(v=>{
             setValue(v);
         })
