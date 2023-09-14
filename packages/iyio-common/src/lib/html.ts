ec10a95ffb333527d449a68c84e73f4fa30eac18
Scott Vance <scott@iyio.io>
2023-09-13 23:13:39 -0400

    - Added escapeHtmlKeepSingleQuote

--- packages/iyio-common/src/lib/html.ts
+++ packages/iyio-common/src/lib/html.ts
@@ -10,3 +10,14 @@ export const escapeHtml=(str:string):string=>
          .replace(/"/g, "&quot;")
          .replace(/'/g, "&#039;");
  }
+export const escapeHtmlKeepSingleQuote=(str:string):string=>
+{
+    if(!str){
+        return '';
+    }
+    return str
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "'")
+ }
