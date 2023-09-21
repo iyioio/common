9706414915ae1c94d94239d341b57a5e946df465
Scott Vance <scott@iyio.io>
2023-09-21 13:51:36 -0400

    - added blob-lib

--- packages/iyio-common/src/index.ts
+++ packages/iyio-common/src/index.ts
@@ -38,6 +38,7 @@ export * from './lib/base-layout';
 export * from './lib/base-layout-generator';
 export * from './lib/base-layout-generator-types';
 export * from './lib/base64';
+export * from './lib/blob-lib';
 export * from './lib/cdk-common-const';
 export * from './lib/cli-parser';
 export * from './lib/client-download';
---
+++ packages/iyio-common/src/lib/blob-lib.ts
@@ -0,0 +1,19 @@
+export interface FileBlobOptions
+{
+    lastModified?:number;
+}
+
+export class FileBlob extends Blob
+{
+    public readonly lastModified:number;
+
+    public override readonly name:string;
+
+    public constructor(parts:BlobPart[],name:string,options:BlobPropertyBag,{
+        lastModified=Date.now()
+    }:FileBlobOptions={}){
+        super(parts,options);
+        this.name=name;
+        this.lastModified=lastModified;
+    }
+}
