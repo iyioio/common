efd41ffb4b9030fd04c172116f391cbb8e95c4a4
Scott Vance <scott@iyio.io>
2023-09-21 13:51:22 -0400

    - Added the MediaRecording class

--- packages/media-common/src/index.ts
+++ packages/media-common/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/MediaRecordering';
 export * from './lib/blurhash';
 export * from './lib/media-lib';
 export * from './lib/media-types';
---
+++ packages/media-common/src/lib/MediaRecordering.ts
@@ -0,0 +1,239 @@
+import { PromiseSource, ReadonlySubject, asArray, createPromiseSource, delayAsync } from "@iyio/common";
+import { BehaviorSubject, Observable, Subject } from "rxjs";
+
+export const defaultMediaRecorderingKBPerSecond=2;
+
+export interface MediaRecorderingOptions
+{
+    recorderOptions?:MediaRecorderOptions;
+    stream?:MediaStream;
+    recorder?:MediaRecorder;
+    /**
+     * The first media constraint that yeilds a stream will be used
+     */
+    mediaConstraints?:(MediaStreamConstraints|null|undefined)|(MediaStreamConstraints|null|undefined)[];
+
+    /**
+     * Default is 2 KB per second
+     */
+    kBPerSecond?:number;
+
+    /**
+     * If ture all recored data blobs will be stored in the data property and returned.
+     */
+    storeAllData?:boolean;
+
+    /**
+     * If defined the recoding will be split into spearte blobs.
+     */
+    timesliceMs?:number;
+
+
+}
+
+export interface MediaRecordingResult
+{
+    data:Blob[];
+    recorder:MediaRecorder;
+    mimeType:string;
+}
+
+export class MediaRecordering
+{
+
+    private readonly options:MediaRecorderingOptions;
+
+    private readonly _recorder:BehaviorSubject<MediaRecorder|null>=new BehaviorSubject<MediaRecorder|null>(null);
+    public get recorderSubject():ReadonlySubject<MediaRecorder|null>{return this._recorder}
+    public get recorder(){return this._recorder.value}
+
+    private readonly _data:BehaviorSubject<Blob[]>=new BehaviorSubject<Blob[]>([]);
+    public get dataSubject():ReadonlySubject<Blob[]>{return this._data}
+    public get data(){return this._data.value}
+
+    private readonly _recording:BehaviorSubject<boolean>=new BehaviorSubject<boolean>(false);
+    public get recordingSubject():ReadonlySubject<boolean>{return this._recording}
+    public get recording(){return this._recording.value}
+
+    private readonly _onData=new Subject<Blob>();
+    public get onData():Observable<Blob>{return this._onData}
+
+
+    public constructor(options:MediaRecorderingOptions)
+    {
+        this.options={...options};
+    }
+
+    private _isDisposed=false;
+    public get isDisposed(){return this._isDisposed}
+    public get isStopped(){return this._isDisposed}
+    public dispose()
+    {
+        if(this._isDisposed){
+            return;
+        }
+        this._isDisposed=true;
+        try{
+            this._recorder.value?.stop();
+        }catch{
+            //
+        }
+        this.endSource?.resolve();
+    }
+
+    private isPaused=false;
+
+    public start()
+    {
+        this.recordAsync();
+    }
+
+    private recordPromise:Promise<MediaRecordingResult|null>|null=null;
+    public async recordAsync():Promise<MediaRecordingResult|null>
+    {
+        if(!this.recordPromise){
+            this.recordPromise=this._recordAsync();
+        }
+        return await this.recordPromise;
+    }
+
+    private endSource:PromiseSource<void>|null=null;
+
+    private async _recordAsync():Promise<MediaRecordingResult|null>
+    {
+        if(this._isDisposed){
+            return null;
+        }
+
+        const getOptions=()=>{
+            const mo:MediaRecorderOptions={
+                bitsPerSecond:(this.options.kBPerSecond??defaultMediaRecorderingKBPerSecond)*8000,
+                ...this.options.recorderOptions,
+            }
+            return mo;
+        }
+
+        let recorder=this.options.recorder;
+        if(!recorder && this.options.stream){
+            recorder=new MediaRecorder(this.options.stream,getOptions());
+        }
+
+        let disposeStream:MediaStream|null=null;
+
+        if(!recorder && this.options.mediaConstraints){
+            const ary=asArray(this.options.mediaConstraints)??[];
+            for(const mc of ary){
+                if(!mc){
+                    continue;
+                }
+                try{
+                    const stream=await navigator.mediaDevices.getUserMedia(mc);
+                    if(!stream){
+                        continue;
+                    }
+                    disposeStream=stream;
+                    recorder=new MediaRecorder(stream,getOptions());
+                    break;
+                }catch{
+                    //
+                }
+            }
+        }
+
+        if(!recorder){
+            throw new Error('Unable to stream for recording');
+        }
+
+        try{
+
+            const data:Blob[]=[];
+
+            if(this._isDisposed){
+                return {
+                    recorder,
+                    data,
+                    mimeType:'',
+                };
+            }
+
+            const endSource=createPromiseSource<void>();
+
+            recorder.addEventListener('dataavailable',e=>{
+                if(!this.options.storeAllData){
+                    data.splice(0,data.length);
+                }
+                data.push(e.data);
+                this._data.next([...data]);
+                this._onData.next(e.data);
+            })
+            recorder.addEventListener('pause',()=>this.isPaused=true);
+            recorder.addEventListener('resume',()=>this.isPaused=false);
+            recorder.addEventListener('stop',()=>{this._recording.next(false);endSource.resolve()});
+            recorder.addEventListener('start',()=>this._recording.next(true));
+            recorder.addEventListener('error',e=>endSource.reject(e));
+
+            if(this.options.timesliceMs){
+                recorder.start(this.options.timesliceMs);
+            }else{
+                recorder.start();
+            }
+            if(this.isPaused){
+                recorder.pause();
+            }
+            this.endSource=endSource;
+            this._recorder.next(recorder);
+
+            await endSource.promise;
+
+            const start=Date.now();
+            while(!data.length && Date.now()-start<2000){
+                await delayAsync(1);
+            }
+
+            return {
+                recorder,
+                data,
+                mimeType:recorder.mimeType,
+            }
+        }finally{
+            if(disposeStream){
+                try{
+                    disposeStream.getTracks().forEach(t=>{
+                        t.stop();
+                    })
+                }catch{
+                    //
+                }
+            }
+            try{
+                this._recorder.value?.stop();
+            }catch{
+                //
+            }
+        }
+    }
+
+    public stop()
+    {
+        this.dispose();
+    }
+
+    public pause()
+    {
+        if(this.isPaused){
+            return;
+        }
+        this.isPaused=true;
+        this._recorder.value?.pause();
+
+    }
+
+    public resume()
+    {
+        if(!this.isPaused){
+            return;
+        }
+        this.isPaused=false;
+        this._recorder.value?.resume();
+    }
+}
