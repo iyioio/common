36b70d71a4f88477cd396787773532c2cec6cce7
Scott Vance <scott@iyio.io>
2023-10-05 22:34:19 -0400

    - Added fitToMaxSize to ScrollView

--- packages/react-common/src/lib/ScrollView.tsx
+++ packages/react-common/src/lib/ScrollView.tsx
@@ -1,6 +1,7 @@
-import { cn } from "@iyio/common";
-import Style from "styled-jsx/style";
+import { atDotCss } from "@iyio/at-dot-css";
+import { useEffect, useState } from "react";
 import { View, ViewProps } from "./View";
+import { useElementSize } from "./useElementSize";
 
 export type ScrollViewDirection='y'|'x'|'both';
 
@@ -10,6 +11,7 @@ export interface ScrollViewProps extends ViewProps
     children?:any;
     direction?:ScrollViewDirection;
     overflowRef?:(elem:HTMLElement|null)=>void;
+    fitToMaxSize?:string;
 }
 
 export function ScrollView({
@@ -20,49 +22,75 @@ export function ScrollView({
     row,
     col,
     overflowRef,
+    fitToMaxSize,
     ...props
 }:ScrollViewProps){
 
+    const [container,setContainer]=useState<HTMLElement|null>(null);
+
+    const [size]=useElementSize(fitToMaxSize?container:undefined);
+
+    const containerRef=containerProps?.elemRef;
+    useEffect(()=>{
+        containerRef?.(container);
+    },[containerRef,container]);
+
     return (
-        <View className={cn("ScrollView",'scroll-'+direction,className)} {...props}>
+        <View
+            className={style.root({[`scroll-${direction}`]:true},className)}
+            style={{
+                maxHeight:(fitToMaxSize && (direction==='y'||direction==='both'))?fitToMaxSize:undefined,
+                maxWidth:(fitToMaxSize && (direction==='x'||direction==='both'))?fitToMaxSize:undefined,
+                height:(fitToMaxSize && size && (direction==='y'||direction==='both'))?size.height+'px':undefined,
+                width:(fitToMaxSize && size && (direction==='x'||direction==='both'))?size.width+'px':undefined,
+                ...props.style
+            }}
+            {...props}
+        >
 
             <div ref={overflowRef}>
-                <View row={row} col={col} {...containerProps}>
+                <View
+                    row={row}
+                    col={col}
+                    {...containerProps}
+                    elemRef={(fitToMaxSize || containerProps?.elemRef)?setContainer:undefined}
+                >
                     {children}
                 </View>
             </div>
-
-            <Style global id="ScrollView-bmpfMZwFK7GneUZQ73Fn" jsx>{`
-                .ScrollView{
-                    position:relative;
-                }
-                .ScrollView.scroll-y > div{
-                    position:absolute;
-                    left:0;
-                    right:0;
-                    top:0;
-                    bottom:0;
-                }
-                .ScrollView.scroll-y > div{
-                    overflow-x:hidden;
-                    overflow-x:clip;
-                    overflow-y:auto;
-                    overflow-y:overlay;
-                }
-                .ScrollView.scroll-x > div{
-                    overflow-x:hidden;
-                    overflow-x:clip;
-                    overflow-y:auto;
-                    overflow-y:overlay;
-                }
-                .ScrollView.scroll-both > div{
-                    overflow-x:auto;
-                    overflow-y:overlay;
-                    overflow-y:auto;
-                    overflow-y:overlay;
-                }
-            `}</Style>
         </View>
     )
 
 }
+
+const style=atDotCss({name:'ScrollView',css:`
+    @.root{
+        position:relative;
+    }
+    @.root.scroll-y > div{
+        position:absolute;
+        left:0;
+        right:0;
+        top:0;
+        bottom:0;
+    }
+    @.root.scroll-y > div{
+        overflow-x:hidden;
+        overflow-x:clip;
+        overflow-y:auto;
+        overflow-y:overlay;
+    }
+    @.root.scroll-x > div{
+        overflow-x:hidden;
+        overflow-x:clip;
+        overflow-y:auto;
+        overflow-y:overlay;
+    }
+    @.root.scroll-both > div{
+        overflow-x:auto;
+        overflow-y:overlay;
+        overflow-y:auto;
+        overflow-y:overlay;
+    }
+
+`});
