b074980c543ea763aea156c70c5d0b96c521cecc
Scott Vance <scott@iyio.io>
2023-10-11 18:45:27 -0400

    - added projectName to next-js-app templ

--- tmpl/next-js-app/jest.config.ts
+++ tmpl/next-js-app/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-    displayName: 'web-app',
+    displayName: '___$projectName___',
     preset: '../../jest.preset.js',
     transform: {
         '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
         '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coverageDirectory: '../../coverage/packages/web-app',
+    coverageDirectory: '../../coverage/packages/___$projectName___',
 };
