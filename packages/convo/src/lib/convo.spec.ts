76844d302738a5a9fc7f7bf1e14d0a60bb0a81f4
Scott Vance <scott@iyio.io>
2023-10-25 20:35:32 -0400

    - updated spacing after plugin update

--- packages/convo/src/lib/convo.spec.ts
+++ packages/convo/src/lib/convo.spec.ts
@@ -109,39 +109,40 @@ describe('convo',()=>{
     }
 
     it('should parse',()=>{
-
         parse(6,defaultPrompt);
-
     })
 
+
+
     it('should generate function scheme',async ()=>{
+
         const convo=parse(1,/*convo*/`
 
-> schemeTestFn(
-    numberProp: number
-    stringProp: string
-    booleanProp: boolean
-    timeProp: time
-    voidProp: void
-    nullProp: null
-    undefinedProp: undefined
-    mapTProp: map
-    arrayTProp: array
-    anyProp: any
-    intProp: int
-    mapProp: map(
-        stringMapProp: string
-        numberMapProp: number
-    )
-    arrayProp: array( string )
-    arrayMapProp: array( map(
-        stringArrayMapProp: string
-        numberArrayMapProp: number
-    ))
-    numberPropOptional?: number
-    stringPropOptional?: string
-)
-        `/*end*/);
+            > schemeTestFn(
+                numberProp: number
+                stringProp: string
+                booleanProp: boolean
+                timeProp: time
+                voidProp: void
+                nullProp: null
+                undefinedProp: undefined
+                mapTProp: map
+                arrayTProp: array
+                anyProp: any
+                intProp: int
+                mapProp: map(
+                    stringMapProp: string
+                    numberMapProp: number
+                )
+                arrayProp: array( string )
+                arrayMapProp: array( map(
+                    stringArrayMapProp: string
+                    numberArrayMapProp: number
+                ))
+                numberPropOptional?: number
+                stringPropOptional?: string
+            )
+        `);
 
         const fn=convo.messages[0]?.fn;
 
@@ -203,21 +204,20 @@ describe('convo',()=>{
 
         (params.arrayMapProp as any[]).push(false);
         expect(args.safeParse(params).success).toBe(false);
-
-
-
-
     })
 
-    it('should add numbers',async ()=>{
-        const convo=parse(1,/*convo*/`
 
-> testFn(
-    valueA: number
-    valueB?: number
-) -> values (
-    return( add(values.valueA values.valueB) )
-)
+
+
+    it('should add numbers',async ()=>{
+
+        const convo=parse(1,/*convo*/`
+            > testFn(
+                valueA: number
+                valueB?: number
+            ) -> values (
+                return( add(values.valueA values.valueB) )
+            )
         `);
 
         const fn=convo.messages[0]?.fn;
@@ -233,12 +233,15 @@ describe('convo',()=>{
 
     })
 
-    it('should escape single quotes in strings',async ()=>{
-        const convo=parse(1,/*convo*/`
 
-> testFn() -> values (
-    return( 'I\\'m bob' )
-)
+
+
+    it('should escape single quotes in strings',async ()=>{
+
+        const convo=parse(1,/*convo*/`
+            > testFn() -> values (
+                return( 'I\\'m bob' )
+            )
         `);
 
         const fn=convo.messages[0]?.fn;
@@ -252,12 +255,16 @@ describe('convo',()=>{
 
     })
 
-    it('should escape double quotes in strings',async ()=>{
-        const convo=parse(1,/*convo*/`
 
-> testFn() -> values (
-    return( "in the famous word of colonel sanders \\"i'm too drunk to taste this chicken\\" " )
-)
+
+
+
+    it('should escape double quotes in strings',async ()=>{
+
+        const convo=parse(1,/*convo*/`
+            > testFn() -> values (
+                return( "in the famous word of colonel sanders \\"i'm too drunk to taste this chicken\\" " )
+            )
         `);
 
         const fn=convo.messages[0]?.fn;
@@ -271,12 +278,16 @@ describe('convo',()=>{
 
     })
 
-    it('should embed strings',async ()=>{
-        const convo=parse(1,/*convo*/`
 
-> testFn() -> values (
-    return( "1 + 1 = {{ add(1 1) }}" )
-)
+
+
+
+    it('should embed strings',async ()=>{
+
+        const convo=parse(1,/*convo*/`
+            > testFn() -> values (
+                return( "1 + 1 = {{ add(1 1) }}" )
+            )
         `);
 
         const fn=convo.messages[0]?.fn;
@@ -287,17 +298,20 @@ describe('convo',()=>{
         }
 
         expect(executeConvoFunction(fn)).toBe("1 + 1 = 2");
-
     })
+
+
+
+
 
     it('should add compare negative numbers',async ()=>{
-        const convo=parse(1,/*convo*/`
 
-> testFn(
-    value: number
-) -> values (
-    return( eq(value -2) )
-)
+        const convo=parse(1,/*convo*/`
+            > testFn(
+                value: number
+            ) -> values (
+                return( eq(value -2) )
+            )
         `);
 
         const fn=convo.messages[0]?.fn;
@@ -312,15 +326,19 @@ describe('convo',()=>{
         expect(executeConvoFunction(fn,{value:2})).toBe(false);
 
     })
+
+
+
+
 
     it('should allow commas and semi-colons',async ()=>{
-        const convo=parse(1,/*convo*/`
 
-> testFn(
-    value: number
-) -> values (
-    return( eq( value , -2) );
-)
+        const convo=parse(1,/*convo*/`
+            > testFn(
+                value: number
+            ) -> values (
+                return( eq( value , -2) );
+            )
         `);
 
         const fn=convo.messages[0]?.fn;
@@ -335,21 +353,24 @@ describe('convo',()=>{
         expect(executeConvoFunction(fn,{value:2})).toBe(false);
 
     })
+
+
+
 
 
     it('should deep return',async ()=>{
-        const convo=parse(1,/*convo*/`
 
-> testFn(
-    value: boolean
-) -> p (
-    if(p.value) then(
-        if(p.value) then(
-            return('first')
-        )
-    )
-    return('last')
-)
+        const convo=parse(1,/*convo*/`
+            > testFn(
+                value: boolean
+            ) -> p (
+                if(p.value) then(
+                    if(p.value) then(
+                        return('first')
+                    )
+                )
+                return('last')
+            )
         `);
 
         const fn=convo.messages[0]?.fn;
@@ -365,18 +386,22 @@ describe('convo',()=>{
 
     })
 
-    it('should return based on if',async ()=>{
-        const convo=parse(1,/*convo*/`
 
-> testFn(
-    value: number
-) -> (
-    if(mt(value 5)) then (
-        return('more')
-    )else(
-        return('less')
-    )
-)
+
+
+
+    it('should return based on if',async ()=>{
+
+        const convo=parse(1,/*convo*/`
+            > testFn(
+                value: number
+            ) -> (
+                if(mt(value 5)) then (
+                    return('more')
+                )else(
+                    return('less')
+                )
+            )
         `);
 
         const fn=convo.messages[0]?.fn;
