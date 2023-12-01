a5ba8b9fe1536e69c3056d459f8583db094ba23f
Scott Vance <scott@iyio.io>
2023-12-01 03:23:33 -0500

    - removed no result top level statement support

--- packages/convo-lang/src/lib/convo-parser.ts
+++ packages/convo-lang/src/lib/convo-parser.ts
@@ -5,7 +5,7 @@ import { ConvoFunction, ConvoMessage, ConvoNonFuncKeyword, ConvoParsingResult, C
 type StringType='"'|"'"|'---'|'>';
 
 const fnMessageReg=/(>)\s*(\w+)?\s+(\w+)\s*([*?!]*)\s*(\()/gs;
-const topLevelMessageReg=/(>)\s*(do|no\s+result|result|define|debug)/gs;
+const topLevelMessageReg=/(>)\s*(do|result|define|debug)/gs;
 const roleReg=/(>)[ \t]*(\w+)[ \t]*([*?!]*)/g;
 
 const statementReg=/([\s\n\r]*[,;]*[\s\n\r]*)((#|\/\/|@|\)|\}\}|\}|\]|<<|>|$)|((\w+|"[^"]*"|'[^']*')(\??):)?\s*(([\w.]+)\s*=)?\s*('|"|-{3,}|[\w.]+\s*(\()|[\w.]+|-?[\d.]+|\{|\[))/gs;
