1a1c9c142d12638d6023356430307570a39ce995
Scott Vance <scott@iyio.io>
2023-12-28 20:42:19 -0500

    - ScrollView auto scroll props

--- packages/react-common/src/lib/ScrollView.tsx
+++ packages/react-common/src/lib/ScrollView.tsx
@@ -1,8 +1,8 @@
 import { atDotCss } from "@iyio/at-dot-css";
-import { cn } from "@iyio/common";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { View, ViewProps } from "./View";
 import { useElementSize } from "./useElementSize";
+
 
 export type ScrollViewDirection='y'|'x'|'both';
 
@@ -14,6 +14,15 @@ export interface ScrollViewProps extends ViewProps
     direction?:ScrollViewDirection;
     overflowRef?:(elem:HTMLElement|null)=>void;
     fitToMaxSize?:string;
+    autoScrollEnd?:boolean;
+    autoScrollTrigger?:any;
+    autoScrollBehavior?:ScrollBehavior;
+    autoScrollDelayMs?:number;
+    autoScrollSelector?:string;
+    autoScrollXOffset?:number;
+    autoScrollYOffset?:number;
+    containerFill?:boolean;
+
 }
 
 export function ScrollView({
@@ -26,20 +35,115 @@ export function ScrollView({
     col,
     overflowRef,
     fitToMaxSize,
+    autoScrollEnd,
+    autoScrollTrigger,
+    autoScrollBehavior='smooth',
+    autoScrollDelayMs=100,
+    elemRef,
+    autoScrollSelector,
+    autoScrollXOffset,
+    autoScrollYOffset,
+    containerFill,
     ...props
 }:ScrollViewProps){
 
+    const [root,setRoot]=useState<HTMLElement|null>(null);
     const [container,setContainer]=useState<HTMLElement|null>(null);
+    const [overflowContainer,setOverflowContainer]=useState<HTMLElement|null>(null);
 
-    const [size]=useElementSize(fitToMaxSize?container:undefined);
+    const [rootSize]=useElementSize(containerFill?root:undefined);
+
+    const [containerSize]=useElementSize(container);
+    const {width,height}=containerSize;
+    const sizeRef=useRef(containerSize);
+    sizeRef.current=containerSize;
+
+    const size=fitToMaxSize?containerSize:undefined;
+
+
+    useEffect(()=>{
+        if(autoScrollTrigger===undefined || !overflowContainer || !root){
+            return;
+        }
+
+        const iv=setTimeout(()=>{
+            autoScrollTo(
+                root,
+                direction,
+                overflowContainer,
+                sizeRef.current.width,
+                sizeRef.current.height,
+                autoScrollBehavior,
+                autoScrollSelector,
+                autoScrollXOffset,
+                autoScrollYOffset,
+            )
+        },autoScrollDelayMs);
+
+        return ()=>{
+            clearTimeout(iv);
+        }
+
+    },[
+        root,
+        direction,
+        autoScrollBehavior,
+        autoScrollTrigger,
+        overflowContainer,
+        autoScrollDelayMs,
+        autoScrollSelector,
+        autoScrollXOffset,
+        autoScrollYOffset,
+    ]);
+
+    useEffect(()=>{
+        if(!autoScrollEnd || !overflowContainer || !root){
+            return;
+        }
+
+        const iv=setTimeout(()=>{
+            autoScrollTo(
+                root,
+                direction,
+                overflowContainer,
+                width,
+                height,
+                autoScrollBehavior,
+                autoScrollSelector,
+                autoScrollXOffset,
+                autoScrollYOffset,
+            )
+        },autoScrollDelayMs);
+
+        return ()=>{
+            clearTimeout(iv);
+        }
+
+    },[
+        root,
+        width,
+        height,
+        direction,
+        autoScrollBehavior,
+        autoScrollEnd,
+        overflowContainer,
+        autoScrollDelayMs,
+        autoScrollSelector,
+        autoScrollXOffset,
+        autoScrollYOffset,
+    ]);
 
     const containerRef=containerProps?.elemRef;
     useEffect(()=>{
         containerRef?.(container);
     },[containerRef,container]);
 
+    const containerMinWidth=containerProps?.style?.minWidth??(containerFill?rootSize.width:undefined);
+    const containerMinHeight=containerProps?.style?.minHeight??(containerFill?rootSize.height:undefined)
+
     return (
         <View
+            elemRef={v=>{elemRef?.(v);setRoot(v)}}
             className={style.root({[`scroll-${direction}`]:true},className)}
             style={{
                 maxHeight:(fitToMaxSize && (direction==='y'||direction==='both'))?fitToMaxSize:undefined,
@@ -51,13 +155,25 @@ export function ScrollView({
             {...props}
         >
 
-            <div ref={overflowRef}>
+            <div ref={v=>{overflowRef?.(v);setOverflowContainer(v)}}>
                 <View
                     row={row}
                     col={col}
-                    className={cn(containerClassName,containerProps?.className)}
+                    className={style.container(null,[containerClassName,containerProps?.className])}
                     {...containerProps}
-                    elemRef={(fitToMaxSize || containerProps?.elemRef)?setContainer:undefined}
+                    style={{
+                        ...containerProps?.style,
+                        minWidth:containerMinWidth,
+                        minHeight:containerMinHeight,
+                        [scrollViewContainerMinWidthCssVar as any]:containerMinWidth===undefined?undefined:containerMinWidth+'px',
+                        [scrollViewContainerMinHeightCssVar as any]:containerMinHeight===undefined?undefined:containerMinHeight+'px',
+                    }}
+                    elemRef={(
+                        fitToMaxSize ||
+                        containerProps?.elemRef ||
+                        autoScrollEnd ||
+                        autoScrollTrigger!==undefined
+                    )?setContainer:undefined}
                 >
                     {children}
                 </View>
@@ -66,6 +182,9 @@ export function ScrollView({
     )
 
 }
+
+export const scrollViewContainerMinWidthCssVar='--scroll-view-container-min-width';
+export const scrollViewContainerMinHeightCssVar='--scroll-view-container-min-height';
 
 const style=atDotCss({name:'ScrollView',css:`
     @.root{
@@ -85,10 +204,10 @@ const style=atDotCss({name:'ScrollView',css:`
         overflow-y:overlay;
     }
     @.root.scroll-x > div{
-        overflow-x:hidden;
-        overflow-x:clip;
-        overflow-y:auto;
-        overflow-y:overlay;
+        overflow-y:hidden;
+        overflow-y:clip;
+        overflow-x:auto;
+        overflow-x:overlay;
     }
     @.root.scroll-both > div{
         overflow-x:auto;
@@ -96,5 +215,39 @@ const style=atDotCss({name:'ScrollView',css:`
         overflow-y:auto;
         overflow-y:overlay;
     }
+    @.container{
+        position:relative;
+    }
 
 `});
+
+const autoScrollTo=(
+    root:HTMLElement,
+    direction:ScrollViewDirection,
+    overflowContainer:HTMLElement,
+    width:number,
+    height:number,
+    autoScrollBehavior:ScrollBehavior,
+    autoScrollSelector?:string,
+    autoScrollXOffset=0,
+    autoScrollYOffset=0,
+)=>{
+
+    if(autoScrollSelector){
+        const elem=root.querySelector(autoScrollSelector);
+        if(elem instanceof HTMLElement){
+            overflowContainer.scrollTo({
+                left:direction==='y'?0:elem.offsetWidth-root.offsetWidth+autoScrollXOffset,
+                top:direction==='x'?0:elem.offsetHeight-root.offsetHeight+autoScrollYOffset,
+                behavior:autoScrollBehavior
+            })
+            return;
+        }
+    }
+
+    overflowContainer.scrollTo({
+        left:width,
+        top:height,
+        behavior:autoScrollBehavior
+    })
+}
