01444c3b1cdcdf9190b60dde7827abf2fefabb36
Scott Vance <scott@iyio.io>
2024-01-02 00:45:48 -0500

    - added absCenter to LoadingDots

--- packages/react-common/src/lib/LoadingDots.tsx
+++ packages/react-common/src/lib/LoadingDots.tsx
@@ -4,15 +4,17 @@ export interface LoadingDotsProps
 {
     className?:string;
     disabled?:boolean;
+    absCenter?:boolean;
 }
 
 export function LoadingDots({
     className,
     disabled,
+    absCenter,
 }:LoadingDotsProps){
 
     return (
-        <svg className={style.root({disabled},className)} width="41" height="10" viewBox="0 0 41 10" fill="none">
+        <svg className={style.root({disabled,absCenter},className)} width="41" height="10" viewBox="0 0 41 10" fill="none">
             <circle style={style.vars({order:0})} cx="5.76367" cy="5" r="5" fill="white"/>
             <circle style={style.vars({order:1})} cx="20.7637" cy="5" r="5" fill="white"/>
             <circle style={style.vars({order:2})} cx="35.7637" cy="5" r="5" fill="white"/>
@@ -29,6 +31,10 @@ const style=atDotCss({name:'LoadingDots',css:`
         100%{opacity:0.5}
     }
 
+    @.root{
+        pointer-events:none;
+    }
+
     @.root circle{
         animation:@@@dot 1s ease-in-out infinite;
         animation-delay: calc(@@order * 150ms);
@@ -36,5 +42,12 @@ const style=atDotCss({name:'LoadingDots',css:`
 
     @.root.disabled circle{
         animation-play-state:paused;
+    }
+
+    @.root.absCenter{
+        position:absolute;
+        left:50%;
+        top:50%;
+        transform:translate(-50%,-50%);
     }
 `});
