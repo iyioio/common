aaaeaed314898a666bc078e8ae2835baa4fdd125
Scott Vance <scott@iyio.io>
2024-02-10 10:58:51 -0500

    - Added setup script for iyio-common-py

--- packages/iyio-common-py/src/iyio_common/__init__.py
+++ packages/iyio-common-py/src/iyio_common/__init__.py
@@ -3,3 +3,4 @@ from .s3_helper import *
 from .sql_helper import *
 from .exec_sql import *
 from .rest_server import *
+from .sqs_helper import *
---
+++ packages/iyio-common-py/src/iyio_common/sqs_helper.py
@@ -0,0 +1,70 @@
+import boto3
+from botocore.config import Config
+import os
+from typing import Any, Callable, List
+
+client = None
+
+def run_sqs(
+    queueUrl:str,
+    handler:Callable[[List[Any]],None],
+    *,
+    maxHandleCount:int=-1,
+    maxMessageCount:int=1,
+    waitSeconds:int=30,
+    visibilityTimeoutSeconds:int=30,
+    exitOnFailure:bool=False
+)->int:
+    """ Dequeues messages from a SQS queue and calls the provided handler"""
+    global client
+
+    if not client:
+        botoConfig = Config(
+            region_name = os.getenv('AWS_REGION'),
+        )
+        client=boto3.client('sqs',config=botoConfig)
+
+
+    handleCount=0
+
+    while maxHandleCount<0 or handleCount<maxHandleCount:
+
+        response=client.receive_message(
+            QueueUrl=queueUrl,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=maxMessageCount,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=visibilityTimeoutSeconds,
+            WaitTimeSeconds=waitSeconds
+        )
+
+        if not response or not ('Messages' in response):
+            continue
+
+        messages=response['Messages']
+
+        success=False
+
+        try:
+            handler(messages)
+            handleCount=handleCount+len(messages)
+            success=True
+        except Exception as err:
+            print('SQS handler failed',err)
+
+        if success:
+            client.delete_message_batch(
+                QueueUrl=queueUrl,
+                Entries=list(map((lambda m:({
+                    'Id': m['ReceiptHandle'],
+                    'ReceiptHandle': m['ReceiptHandle']
+                })),messages))
+            )
+        elif exitOnFailure:
+            break
+
+    return handleCount
