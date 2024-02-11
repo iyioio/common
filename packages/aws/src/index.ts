9f7b4ebfb41bf1b9f7c8d2e64c348bc5fbf7430d
Scott Vance <scott@iyio.io>
2024-02-10 22:36:37 -0500

    - Made iyio-util-fns an implicit dependency of cdk-common

--- packages/aws/src/index.ts
+++ packages/aws/src/index.ts
@@ -1,5 +1,6 @@
-export * from './lib/aws-auth';
 export * from './lib/AwsHttpRequestSigner';
 export * from './lib/_modules.aws';
 export * from './lib/_types.aws';
+export * from './lib/aws-auth';
+export * from './lib/cdk-custom-resource-types';
 
