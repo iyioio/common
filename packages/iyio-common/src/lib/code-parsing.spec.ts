ab1ee481ec8769716254d1862ae154f454e64a1d
Scott Vance <scott@iyio.io>
2024-08-04 14:16:37 -0400

    - Added setIndentation

---
+++ packages/iyio-common/src/lib/code-parsing.spec.ts
@@ -0,0 +1,131 @@
+import { setIndentation } from "./code-parsing"
+
+describe('code-parsing',()=>{
+
+    it('should set indentation',()=>{
+
+        const ind0=
+`const fn=()=>{
+    const a=1;
+    const b=1;
+    return a+b;
+}`
+
+        const ind1=
+`    const fn=()=>{
+        const a=1;
+        const b=1;
+        return a+b;
+    }`
+
+        const ind2=
+`        const fn=()=>{
+            const a=1;
+            const b=1;
+            return a+b;
+        }`
+
+        const ind3=
+`            const fn=()=>{
+                const a=1;
+                const b=1;
+                return a+b;
+            }`
+
+
+        const tabSize=4;
+
+        expect(setIndentation(0*tabSize,ind0)).toBe(ind0);
+        expect(setIndentation(1*tabSize,ind0)).toBe(ind1);
+        expect(setIndentation(2*tabSize,ind0)).toBe(ind2);
+        expect(setIndentation(3*tabSize,ind0)).toBe(ind3);
+
+        expect(setIndentation(0*tabSize,ind1)).toBe(ind0);
+        expect(setIndentation(1*tabSize,ind1)).toBe(ind1);
+        expect(setIndentation(2*tabSize,ind1)).toBe(ind2);
+        expect(setIndentation(3*tabSize,ind1)).toBe(ind3);
+
+        expect(setIndentation(0*tabSize,ind2)).toBe(ind0);
+        expect(setIndentation(1*tabSize,ind2)).toBe(ind1);
+        expect(setIndentation(2*tabSize,ind2)).toBe(ind2);
+        expect(setIndentation(3*tabSize,ind2)).toBe(ind3);
+
+        expect(setIndentation(0*tabSize,ind3)).toBe(ind0);
+        expect(setIndentation(1*tabSize,ind3)).toBe(ind1);
+        expect(setIndentation(2*tabSize,ind3)).toBe(ind2);
+        expect(setIndentation(3*tabSize,ind3)).toBe(ind3);
+    })
+
+    it('should set indentation with incurrent source indentation',()=>{
+
+        const ind0=
+`const fn=()=>{
+    const a=1;
+    const b=1;
+    return a+b;
+}`
+
+        const ind1=
+`    const fn=()=>{
+        const a=1;
+        const b=1;
+return a+b;
+    }`
+
+        const ind2=
+`        const fn=()=>{
+            const a=1;
+            const b=1;
+            return a+b;
+        }`
+
+        const ind3=
+`            const fn=()=>{
+                const a=1;
+                const b=1;
+                return a+b;
+            }`
+
+
+        const tabSize=4;
+
+        let src:string;
+        let exp:string;
+
+        src=
+`    const fn=()=>{
+        const a=1;
+        const b=1;
+return a+b;
+    }`
+
+        exp=
+`        const fn=()=>{
+            const a=1;
+            const b=1;
+    return a+b;
+        }`
+
+        expect(setIndentation(2*tabSize,src)).toBe(exp);
+
+
+
+
+        src=
+`            const fn=()=>{
+                const a=1;
+                const b=1;
+  return a+b;
+            }`
+
+        exp=
+`const fn=()=>{
+    const a=1;
+    const b=1;
+return a+b;
+}`
+
+        expect(setIndentation(0*tabSize,src)).toBe(exp);
+    })
+
+})
--- packages/iyio-common/src/lib/code-parsing.ts
+++ packages/iyio-common/src/lib/code-parsing.ts
@@ -41,3 +41,83 @@ export const getLineNumber=(code:string,stopIndex=code.length-1)=>{
     }
     return lineNumber;
 }
+
+export const getLineIndentation=(code:string,startIndex=0):number=>{
+    for(let i=startIndex;i<code.length;i++){
+        if(code[i]!==' '){
+            return i-startIndex;
+        }
+    }
+    return 0;
+}
+
+export interface SetIndentationOptions
+{
+    useBaseFromFirstNonZero?:boolean;
+    noTrim?:boolean;
+}
+export const setIndentation=(indent:number,code:string,{
+    useBaseFromFirstNonZero=false,
+    noTrim=false
+}:SetIndentationOptions={}):string=>{
+
+
+    const lines:string[]=code.split('\n');
+
+    if(!noTrim){
+        while(lines.length && spReg.test(lines[0] as string)){
+            lines.shift();
+        }
+        while(lines.length && spReg.test(lines[lines.length-1] as string)){
+            lines.pop();
+        }
+    }
+
+    let baseInd:number;
+    let alignStart=0;
+    if(useBaseFromFirstNonZero){
+        baseInd=0;
+        for(let i=0;i<lines.length;i++){
+            baseInd=getLineIndentation(lines[i] as string);
+            if(baseInd){
+                alignStart=i;
+                break;
+            }
+        }
+    }else{
+        baseInd=getLineIndentation(lines[0] as string);
+    }
+
+    const indSp=' '.repeat(indent);
+    const negIndent=-indent;
+
+    for(let i=0;i<lines.length;i++){
+        let line=lines[i] as string;
+        const ind=getLineIndentation(line);
+
+        if(ind){
+            line=line.substring(ind);
+        }
+
+
+        if(ind<baseInd && i<alignStart){
+            lines[i]=indSp+line;
+            continue;
+        }
+
+        const diff=ind-baseInd;
+        if(diff<negIndent){
+            lines[i]=line;
+
+        }else{
+            lines[i]=' '.repeat(indent+diff)+line;
+        }
+
+    }
+
+    return lines.join('\n');
+
+
+}
+
+const spReg=/^\s*$/;
