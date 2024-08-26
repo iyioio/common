ce4570447626fe87ac7974a79919119f3247371b
Scott Vance <scott@iyio.io>
2024-08-26 05:28:53 -0400

    - Added defaultData to Form component

--- packages/react-common/src/lib/Form.tsx
+++ packages/react-common/src/lib/Form.tsx
@@ -1,10 +1,11 @@
 import { BaseLayoutProps, bcn } from "@iyio/common";
-import { CSSProperties } from "react";
+import { CSSProperties, useRef } from "react";
 
 export interface FormProps
 {
     onSubmit?:()=>void;
     onSubmitData?:(data:Record<string,any>)=>void;
+    defaultData?:Record<string,any>;
     children?:any;
     style?:CSSProperties;
 
@@ -13,10 +14,14 @@ export interface FormProps
 export function Form({
     onSubmit,
     onSubmitData,
+    defaultData,
     children,
     style,
     ...props
 }:FormProps & BaseLayoutProps){
+
+    const refs=useRef({defaultData});
+    refs.current.defaultData=defaultData;
 
     return (
         <form style={style} onSubmit={e=>{
@@ -24,7 +29,7 @@ export function Form({
             onSubmit?.();
             if(onSubmitData && e.target instanceof HTMLFormElement){
                 const data:any=new FormData(e.target);
-                const value=Object.fromEntries(data.entries());
+                const value={...refs.current.defaultData,...Object.fromEntries(data.entries())};
                 onSubmitData(value);
             }
         }} className={bcn(props,"Form")}>
