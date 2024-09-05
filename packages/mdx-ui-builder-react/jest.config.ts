f9da94924b4f08177fd5b424621ca31cf8956a80
Scott Vance <scott@iyio.io>
2024-09-05 11:40:16 -0400

    - init mdx-ui-builder-react

---
+++ packages/mdx-ui-builder-react/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+    displayName: 'mdx-ui-builder-react',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'node',
+    transform: {
+        '^.+.[tj]s$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.spec.json' },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/packages/mdx-ui-builder-react',
+};
