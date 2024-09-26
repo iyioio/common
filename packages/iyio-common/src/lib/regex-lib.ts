65b898647b39acbf757e170fffe06e03a3231540
Scott Vance <scott@iyio.io>
2024-09-25 20:48:32 -0400

    - added star string reg caching and a function to get a patter string

--- packages/iyio-common/src/lib/regex-lib.ts
+++ packages/iyio-common/src/lib/regex-lib.ts
@@ -23,15 +23,21 @@ export const starStringToRegex=(
     flags?:string,
     startEndOfInput:StarStringStartEndOfInput='both',
 ):RegExp=>{
+    return new RegExp(starStringToRegexString(starString,startEndOfInput),flags);
+}
+
+export const starStringToRegexString=(
+    starString:string,
+    startEndOfInput:StarStringStartEndOfInput='both',
+):string=>{
     const parts=starString.split('*');
     for(let i=0;i<parts.length;i++){
         parts[i]=escapeRegex(parts[i] as string);
     }
-    return new RegExp(
+    return (
         (startEndOfInput==='both' || startEndOfInput==='start'?'^':'')+
         parts.join('.*?')+
-        (startEndOfInput==='both' || startEndOfInput==='end'?'$':''),
-        flags
+        (startEndOfInput==='both' || startEndOfInput==='end'?'$':'')
     )
 }
 
@@ -43,7 +49,7 @@ export const starStringTest=(
 ):boolean=>{
     try{
         if(starString.includes('*')){
-            return starStringToRegex(starString).test(value);
+            return starStringToRegex(starString,flags,startEndOfInput).test(value);
         }
         if(flags?.includes('i')){
             return starString.localeCompare(value,undefined,{sensitivity:'accent'})===0
@@ -54,4 +60,45 @@ export const starStringTest=(
         console.error(`Invalid starString - (${starString})`)
         return false;
     }
+}
+
+export const starStringTestCached=(
+    cacheObj:Record<string,any>,
+    starString:string,
+    value:string,
+    flags?:string,
+    startEndOfInput:StarStringStartEndOfInput='both'
+):boolean=>{
+    try{
+        if(starString.includes('*')){
+            const cache=getCache();
+            let reg=cache.get(cacheObj);
+            if(!reg || reg.pattern!==starString){
+                reg={
+                    reg:starStringToRegex(starString,flags,startEndOfInput),
+                    pattern:starString
+                }
+                cache.set(cacheObj,reg);
+            }
+            return reg.reg.test(value);
+        }
+        if(flags?.includes('i')){
+            return starString.localeCompare(value,undefined,{sensitivity:'accent'})===0
+        }else{
+            return starString===value;
+        }
+    }catch(ex){
+        console.error(`Invalid starString - (${starString})`)
+        return false;
+    }
+}
+
+let regCache:WeakMap<any,CachedReg>|undefined=undefined;
+const getCache=()=>{
+    return regCache??(regCache=new WeakMap());
+}
+interface CachedReg
+{
+    reg:RegExp;
+    pattern:string;
 }
