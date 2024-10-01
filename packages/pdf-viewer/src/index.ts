d810956386417e57638befab2ab38188c1217176
Scott Vance <scott@iyio.io>
2024-09-30 23:16:51 -0400

    - pdf-snapshot

--- packages/pdf-viewer/src/index.ts
+++ packages/pdf-viewer/src/index.ts
@@ -4,4 +4,5 @@ export * from './lib/PdfReaderPool';
 export * from './lib/PdfView';
 export * from './lib/pdf-hooks';
 export * from './lib/pdf-lib';
+export * from './lib/pdf-snapshot';
 
---
+++ packages/pdf-viewer/src/lib/pdf-snapshot.ts
@@ -0,0 +1,51 @@
+import { Lock } from "@iyio/common";
+import { pdfReaderPool } from "./PdfReaderPool";
+
+const runLock=new Lock(1);
+
+const cacheTtl=30000;
+
+export const getPdfSnapshotAsync=async (urlOrFile:string|File,pageIndex=0,format='image/jpeg',allowParallel?:boolean)=>{
+    if(urlOrFile instanceof File){
+        return getSnapAsync(urlOrFile,pageIndex,format);
+    }else if(allowParallel){
+        return await getCachedSnapAsync(urlOrFile,pageIndex,format);
+    }else{
+        const release=await runLock.waitAsync();
+        try{
+            return await getCachedSnapAsync(urlOrFile,pageIndex,format);
+        }finally{
+            release();
+        }
+    }
+}
+
+const cache:Record<string,Promise<Blob|undefined>>={};
+
+const getCachedSnapAsync=async (url:string,pageIndex:number,format:string):Promise<Blob|undefined>=>{
+    const key=`${pageIndex}:${format}:${url}`;
+    const match=cache[key];
+    if(match){
+        return await match;
+    }
+    const p=getSnapAsync(url,pageIndex,format);
+    cache[key]=p;
+    setTimeout(()=>{
+        if(cache[key]===p){
+            delete cache[key];
+        }
+    },cacheTtl);
+    return await p;
+}
+
+const getSnapAsync=async (urlOrFile:string|File,pageIndex:number,format:string):Promise<Blob|undefined>=>{
+    const reader=pdfReaderPool().getReader(urlOrFile);
+    try{
+
+        const blob=await reader.pageToImageAsync(pageIndex,format);
+        return blob??undefined;
+
+    }finally{
+        pdfReaderPool().returnReader(reader);
+    }
+}
