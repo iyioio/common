c2c9c856f91dcefed190b7b78c52ed0c2fb364a8
Scott Vance <scott@iyio.io>
2024-09-30 23:17:48 -0400

    - Image now allows selecting what type of element is renders as

--- packages/react-common/src/lib/Image.tsx
+++ packages/react-common/src/lib/Image.tsx
@@ -1,5 +1,5 @@
 import { baseLayoutCn, BaseLayoutProps, cn } from "@iyio/common";
-import { CSSProperties } from "react";
+import { createElement, CSSProperties } from "react";
 
 export interface ImageProps extends BaseLayoutProps
 {
@@ -16,12 +16,16 @@ export interface ImageProps extends BaseLayoutProps
     tile?:boolean;
     bgSrc?:string|null;
     elemRef?:(elem:HTMLElement|null)=>void;
+    onError?:(evt:any)=>void;
+    onLoad?:(evt:any)=>void;
     unstyled?:boolean;
     sq?:boolean;
     landscape?:boolean;
     portrait?:boolean;
     aspectRatio?:string|number;
     imgFixed?:boolean;
+    useImgElem?:boolean;
+    elemType?:string;
 }
 
 export function Image({
@@ -44,30 +48,40 @@ export function Image({
     portrait,
     aspectRatio=landscape?'16/9':portrait?'9/16':sq?1:undefined,
     imgFixed,
+    onError,
+    onLoad,
+    useImgElem,
+    elemType=useImgElem?'img':'div',
     ...props
 }:ImageProps){
 
-    return (
-        <div
-            title={title}
-            ref={elemRef}
-            role="img"
-            aria-label={alt}
-            className={cn("Image",{contain,tile},baseLayoutCn(props))}
-            style={{
-                [unstyled?'backgroundImage':'background']:unstyled?(
-                    `url(${src})${bgSrc?` ,url(${bgSrc})`:''}`
-                ):(
-                    `${imgFixed?'fixed ':''}url(${src}) center/${tile?'auto':contain?'contain':'cover'} ${tile?'repeat':'no-repeat'}${bgSrc
-                    ?` ,url(${bgSrc}) center/${bgContain?'contain':'cover'} no-repeat`:''}`
-                ),
-                height,
-                width,
-                aspectRatio,
-                ...style
-            }}>
-            {children}
-        </div>
-    )
+    const elemProps={
+        title:title,
+        ref:elemRef,
+        role:'img',
+       'aria-label':alt,
+        className:cn("Image",{contain,tile},baseLayoutCn(props)),
+        onError:onError,
+        onLoad:onLoad,
+        style:{
+            height,
+            width,
+            aspectRatio,
+            ...style
+        }
+    }
+
+    if(elemType==='img'){
+        (elemProps as any).src=src;
+    }else{
+        elemProps.style[unstyled?'backgroundImage':'background']=unstyled?(
+            `url(${src})${bgSrc?` ,url(${bgSrc})`:''}`
+        ):(
+            `${imgFixed?'fixed ':''}url(${src}) center/${tile?'auto':contain?'contain':'cover'} ${tile?'repeat':'no-repeat'}${bgSrc
+            ?` ,url(${bgSrc}) center/${bgContain?'contain':'cover'} no-repeat`:''}`
+        )
+    }
+
+    return createElement(elemType,elemProps,children);
 
 }
