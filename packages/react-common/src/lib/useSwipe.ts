ea23401e1ec131895ed496cc817fa8e1b70dccc0
Scott Vance <scott@iyio.io>
2024-09-30 21:59:01 -0400

    - useSwipe now disabled children move events while swiping

--- packages/react-common/src/lib/useSwipe.ts
+++ packages/react-common/src/lib/useSwipe.ts
@@ -1,4 +1,5 @@
-import { Direction, Point } from "@iyio/common";
+import { atDotCss } from "@iyio/at-dot-css";
+import { Direction, DisposeCallback, Point } from "@iyio/common";
 import { useEffect, useRef, useState } from "react";
 import { DirectionInputCtrl } from "./direction-input/DirectionInputCtrl";
 import { useDirectionInputCtrl } from "./direction-input/direction-input-lib";
@@ -7,8 +8,12 @@ export type SwipeDirection=Direction;
 
 export type SwipeListener=(direction:SwipeDirection)=>void;
 
-export const useSwipe=(listener?:SwipeListener|DirectionInputCtrl,swipeTimeout=2000,swipeDist=30):((listenTo:HTMLElement|null)=>void)=>
-{
+export const useSwipe=(
+    listener?:SwipeListener|DirectionInputCtrl,
+    swipeTimeout=2000,
+    swipeDist=30
+):((listenTo:HTMLElement|null)=>void)=>{
+
     const [listenTo,setListenTo]=useState<HTMLElement|null>(null);
     const dirCtrl=useDirectionInputCtrl();
 
@@ -24,13 +29,26 @@ export const useSwipe=(listener?:SwipeListener|DirectionInputCtrl,swipeTimeout=2
             return;
         }
 
+        style.root();
+
         let posDown={x:0,y:0}
         let time=0;
         let useTouch:boolean|null=null;
+        let isMoving=false;
+        let moveId=0;
+        let moveCleanupIv:any;
+        let moveCleanup:DisposeCallback|undefined;
 
         const touchDown=(e:TouchEvent)=>{
             useTouch=true;
             down({x:e.changedTouches[0]?.clientX??0,y:e.changedTouches[0]?.clientY??0})
+        }
+
+        const touchMove=(e:TouchEvent)=>{
+            if(isMoving || Date.now()-time>swipeTimeout){
+                return;
+            }
+            move({x:e.changedTouches[0]?.clientX??0,y:e.changedTouches[0]?.clientY??0});
         }
 
         const touchUp=(e:TouchEvent)=>{
@@ -43,6 +61,13 @@ export const useSwipe=(listener?:SwipeListener|DirectionInputCtrl,swipeTimeout=2
                 return;
             }
             down({x:e.clientX,y:e.clientY})
+        }
+
+        const mouseMove=(e:MouseEvent)=>{
+            if(useTouch || isMoving || Date.now()-time>swipeTimeout){
+                return;
+            }
+            move({x:e.clientX,y:e.clientY});
         }
 
         const mouseUp=(e:MouseEvent)=>{
@@ -80,7 +105,9 @@ export const useSwipe=(listener?:SwipeListener|DirectionInputCtrl,swipeTimeout=2
                 return;
             }
 
+            time=0;
             e.preventDefault();
+            moveCleanup?.();
 
             const dir:Direction=(
                  xDiffAbs>yDiffAbs?
@@ -99,19 +126,61 @@ export const useSwipe=(listener?:SwipeListener|DirectionInputCtrl,swipeTimeout=2
 
         }
 
+        const move=(pt:Point)=>{
+            const xDiff=pt.x-posDown.x;
+            const yDiff=pt.y-posDown.y;
+            const xDiffAbs=Math.abs(xDiff);
+            const yDiffAbs=Math.abs(yDiff);
+
+            if(xDiffAbs<swipeDist && yDiffAbs<swipeDist){
+                return;
+            }
+            moveCleanup?.();
+
+            isMoving=true;
+            listenTo.classList.add(style.swiping());
+            const mId=++moveId;
+
+            let disposed=false;
+            const cleanup=()=>{
+                clearTimeout(moveCleanupIv);
+                if(disposed || moveId!==mId){
+                    return;
+                }
+                disposed=true;
+                isMoving=false;
+                listenTo.classList.remove(style.swiping());
+                moveCleanup=undefined;
+            }
+            moveCleanup=cleanup;
+            moveCleanupIv=setTimeout(cleanup,swipeTimeout+100);
+
+        }
+
         listenTo.addEventListener('touchstart',touchDown);
         listenTo.addEventListener('touchend',touchUp);
         listenTo.addEventListener('mousedown',mouseDown);
         listenTo.addEventListener('mouseup',mouseUp);
+        listenTo.addEventListener('mousemove',mouseMove);
+        listenTo.addEventListener('touchmove',touchMove);
 
         return ()=>{
             listenTo.removeEventListener('touchstart',touchDown);
             listenTo.removeEventListener('touchend',touchUp);
             listenTo.removeEventListener('mousedown',mouseDown);
             listenTo.removeEventListener('mouseup',mouseUp);
+            listenTo.removeEventListener('mousemove',mouseMove);
+            listenTo.removeEventListener('touchmove',touchMove);
         }
 
     },[listenTo])
 
     return setListenTo;
 }
+
+const style=atDotCss({name:'useSwipe',namespace:'iyio',order:'framework',css:`
+    @.swiping > *{
+        pointer-events:none !important;
+    }
+
+`});
