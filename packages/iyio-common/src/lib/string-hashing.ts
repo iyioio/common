36b80f20b6be46c44a4cdc0e37b3425f280d6005
Scott Vance <scott@iyio.io>
2024-11-10 18:46:00 -0500

    - added strAryHashToAry for creating hashes from string arrays

--- packages/iyio-common/src/lib/string-hashing.ts
+++ packages/iyio-common/src/lib/string-hashing.ts
@@ -33,10 +33,35 @@ export const strHashToAry=(str:string,fillAry:Uint32Array,aryLen=fillAry.length)
     }
 }
 
+export const strAryHashToAry=(strings:string[],fillAry:Uint32Array,aryLen=fillAry.length):void=>{
+    fillAry.fill(0);
+    if(!fillAry.length){
+        return;
+    }
+    let allI=0;
+    for(let s=0;s<strings.length;s++){
+        const str=strings[s]??'';
+        const l=str.length;
+        for (let i=0,c:number;i<l;i++) {
+            c=str.charCodeAt(i);
+            const n=allI%aryLen;
+            let h=fillAry[n] as number;
+            h=((h<<5)-h)+c;
+            h|=0; // Convert to 32bit integer
+            fillAry[n]=h;
+            allI++;
+        }
+    }
+}
 
-export const strHashBase64=(str:string,length=8,keyStr:string=defaultBase64Chars):string=>{
+
+export const strHashBase64=(str:string|string[],length=8,keyStr:string=defaultBase64Chars):string=>{
     const fillAry=new Uint32Array(length);
-    strHashToAry(str,fillAry);
+    if(Array.isArray(str)){
+        strAryHashToAry(str,fillAry);
+    }else{
+        strHashToAry(str,fillAry);
+    }
     let len=fillAry.length;
     while(fillAry[len-1]===0){
         len--;
@@ -44,6 +69,6 @@ export const strHashBase64=(str:string,length=8,keyStr:string=defaultBase64Chars
     return base64EncodeUint32Array(fillAry,keyStr,len);
 }
 
-export const strHashBase64Fs=(str:string,length=8,keyStr:string=fsBase64Chars):string=>{
+export const strHashBase64Fs=(str:string|string[],length=8,keyStr:string=fsBase64Chars):string=>{
     return strHashBase64(str,length,keyStr);
 }
