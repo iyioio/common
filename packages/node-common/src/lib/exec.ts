2a4308b3dfb6b6b5fab37445fe6a6d0469511c31
Scott Vance <scott@iyio.io>
2024-12-12 23:21:05 -0500

    - Added spawnAsync

--- packages/node-common/src/lib/exec.ts
+++ packages/node-common/src/lib/exec.ts
@@ -1,4 +1,4 @@
-import { exec } from 'node:child_process';
+import { ChildProcess, ChildProcessWithoutNullStreams, exec, spawn } from 'node:child_process';
 
 export interface ExecOptions
 {
@@ -107,5 +107,85 @@ export const execAsync:execAsyncOverrides=(
                 r(_stdout?.trim()||'');
             }
         })
+    })
+}
+
+export interface SpawnOptions
+{
+    cmd:string;
+    cwd?:string;
+    out?:(value:string)=>void;
+    outPrefix?:string;
+    silent?:boolean;
+    throwOnError?:boolean;
+    stdout?:(...value:string[])=>void;
+    stderr?:(...value:string[])=>void;
+    onChild?:(proc:ChildProcess)=>void;
+    onOutput?:(type:string,value:string)=>void;
+    onError?:(type:string,value:string)=>void;
+}
+
+export const spawnAsync=(
+    cmdOrOptions:SpawnOptions|string,
+    _silent=false,
+    _throwOnError=false
+)=>{
+    const options:SpawnOptions=typeof cmdOrOptions === 'string'?{
+        cmd:cmdOrOptions,
+        silent:_silent,
+        throwOnError:_throwOnError,
+    }:cmdOrOptions;
+
+    const {
+        cmd,
+        cwd,
+        out,
+        silent=out?true:false,
+        stdout=console.info,
+        stderr=console.warn,
+        onChild,
+        onOutput,
+        onError,
+        outPrefix='',
+        throwOnError
+    }=options;
+
+    return new Promise((r,j)=>{
+        if(!silent){
+            stdout('> '+cmd);
+        }
+        out?.('> '+cmd)
+        let child:ChildProcessWithoutNullStreams|undefined=undefined;
+
+        child=spawn(cmd,{cwd,shell:true});
+        child.on('error',j);
+        child.on('exit',code=>code?j(code):r(''));
+        child.on('disconnect',()=>r(''));
+        child.on('close',()=>r(''));
+        child.stdout.setEncoding('utf8');
+        child.stdout.on('data',(data)=>{
+            if(typeof data !== 'string'){
+                data=data?.toString()??''
+            }
+            if(onOutput){
+                onOutput('out',data)
+            }
+            stdout(outPrefix?outPrefix+data:data);
+        });
+        child.stderr.setEncoding('utf8');
+        child.stderr.on('data',(data)=>{
+            if(typeof data !== 'string'){
+                data=data?.toString()??''
+            }
+            if(onError){
+                onError('err',data)
+            }
+            stderr(outPrefix?outPrefix+data:data);
+            if(throwOnError){
+                j(data);
+                child?.kill();
+            }
+        });
+        onChild?.(child)
     })
 }
