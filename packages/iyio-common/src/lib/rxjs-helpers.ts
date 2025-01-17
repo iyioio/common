d0f3d94ebcac6636847cfd6c5e7d5c020ebb3b67
Scott Vance <scott@iyio.io>
2025-01-17 13:21:21 -0500

    - added more subject helpers

--- packages/iyio-common/src/lib/rxjs-helpers.ts
+++ packages/iyio-common/src/lib/rxjs-helpers.ts
@@ -1,7 +1,19 @@
 import { BehaviorSubject } from "rxjs";
+import { DisposeCallback } from "./common-types";
 
 export const incrementBehaviourSubject=(subject:BehaviorSubject<number>):number=>{
     const n=subject.value+1;
     subject.next(n);
     return n;
+}
+export const decrementBehaviourSubject=(subject:BehaviorSubject<number>):number=>{
+    const n=subject.value-1;
+    subject.next(n);
+    return n;
+}
+export const incrementBehaviourSubjectWithDispose=(subject:BehaviorSubject<number>):DisposeCallback=>{
+    subject.next(subject.value+1);
+    return ()=>{
+        subject.next(subject.value-1);
+    }
 }
