5ee229dc0d595c082a75244676c3962b1744ec4f
Scott Vance <scott@iyio.io>
2025-05-06 10:51:05 -0400

    - added supClients for different roles

--- packages/supabase-common/src/lib/supClient.ts
+++ packages/supabase-common/src/lib/supClient.ts
@@ -1,7 +1,25 @@
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
-import { supabaseAnonKeyParam, supabaseApiUrlParam } from './supabase-common.deps';
+import { supabaseAnonKeyParam, supabaseApiUrlParam, supabaseServiceRoleKeyParam } from './supabase-common.deps';
 
-let client:SupabaseClient|null=null;
+/**
+ * Returns the service client if the service role key param is defined, otherwise the user client is returned
+ */
 export const supClient=():SupabaseClient=>{
-    return client??(client=createClient(supabaseApiUrlParam(),supabaseAnonKeyParam()));
+    return supabaseServiceRoleKeyParam.get()?supServiceClient():supUserClient();
+}
+
+let userClient:SupabaseClient|null=null;
+/**
+ * Returns the Supabase client for the current signed in user or the anon user
+ */
+export const supUserClient=():SupabaseClient=>{
+    return userClient??(userClient=createClient(supabaseApiUrlParam(),supabaseAnonKeyParam()));
+}
+
+let serviceClient:SupabaseClient|null=null;
+/**
+ * Returns the Supabase client using the service role key
+ */
+export const supServiceClient=():SupabaseClient=>{
+    return serviceClient??(serviceClient=createClient(supabaseApiUrlParam(),supabaseServiceRoleKeyParam()));
 }
--- packages/supabase-common/src/lib/supabase-common.deps.ts
+++ packages/supabase-common/src/lib/supabase-common.deps.ts
@@ -2,3 +2,4 @@ import { defineStringParam } from "@iyio/common";
 
 export const supabaseApiUrlParam=defineStringParam('supabaseApiUrl');
 export const supabaseAnonKeyParam=defineStringParam('supabaseAnonKey');
+export const supabaseServiceRoleKeyParam=defineStringParam('supabaseServiceRoleKey');
