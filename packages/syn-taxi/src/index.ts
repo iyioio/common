7ae15a85ce73bf0260fd4fa95cb60d622f511fcf
Scott Vance <scott@iyio.io>
2025-08-01 12:56:37 -0400

    - export grammars

--- packages/syn-taxi/src/index.ts
+++ packages/syn-taxi/src/index.ts
@@ -2,5 +2,8 @@ export * from './lib/CodeInput';
 export * from './lib/CodeView';
 export * from './lib/LazyCodeInput';
 export * from './lib/LazyCodeView';
+export * from './lib/grammar/atDotCss';
+export * from './lib/grammar/convo';
+export * from './lib/grammar/mdx';
 export * from './lib/shiki';
 
