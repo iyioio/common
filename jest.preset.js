fb986f4e093533cc5d414428bbf2683bcf626c4a
Scott Vance <scott@iyio.io>
2025-10-22 09:55:10 -0400

    - deleted old just config

--- jest.preset.js
+++
@@ -1,8 +0,0 @@
-const nxPreset = require('@nrwl/jest/preset').default;
-
-module.exports = {
-    ...nxPreset,
-    moduleNameMapper:{
-        '#node-web-compat':'./node-web-compat-node.js',
-    },
-};
