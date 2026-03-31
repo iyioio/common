b74f2cfa20461830e082dbc0856f76292b3ec6ce
Scott Vance <scott@iyio.io>
2026-03-31 16:46:35 -0400

    - Added toBasePath to UIActionItem

--- packages/iyio-common/src/lib/ui-lib.ts
+++ packages/iyio-common/src/lib/ui-lib.ts
@@ -19,6 +19,7 @@ export interface UiActionItem<
     icon?:TIcon;
     iconColor?:string;
     to?:string;
+    toBasePath?:string;
     openLinkInNewWindow?:boolean;
     type?:TType;
     action?:(item:UiActionItem<any,any,any>)=>void;
